16ae4bfcf1e986f4e93e26c4934c030f46f58ef6
erica0501-wng <ericaaawonggg0501@gmail.com>
2026-02-23 11:37:35 +0800

    Add missing Transaction table migration

---
+++ backend/prisma/migrations/20260223_add_transaction_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('TopUp', 'Withdraw');
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "paymentMethod" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
